e82c763d9a478781e41efd9e65bbdc01d0169ea1
kuanchiliao1 <kuanchiliao@gmail.com>
2023-02-11 19:32:16 -0500

    Test netlify env variables

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-/*
-Game plan:
-- initialize the generate btn
-- initialize the form
-- add event listener to the generate btn
-  - prevent default
-  - call openAI api
-    - loading animation/picture while we wait
-      consider getting text token by token
-    - 
-  - clear form
-- display modal with the generated text
-- count characters
-  - add eventlisteners to both inputs to constantly listen for any keypress
-    - on every keypress
-      - count all the chars in both inputs
-      - counts all the words in both inputs
-*/
-
 // You must get your own API key from open AI and make your own API_key.js file!
-import secretKey from "./API_key.js"
-console.log(secretKey)
+// import secretKey from "./API_key.js"
+
+exports.handler = async () => {
+  const mySecret = process.env.MY_SECRET;
+  console.log(mySecret)
+  return {
+    statusCode: 200,
+    body: `hello world! I have a ${mySecret}`,
+  };
+};
+
+const secretKey = "123"
 
 const generateBtn = document.getElementById('generate-btn');
 const form = document.getElementById('form');
 const formData = new FormData(form)
-const letterInfo = {
-  giver: "",
-  receiver: "",
-  notes: "",
-  length: ""
-}
+const letterInfo = { giver, receiver, notes, length }
 const slider = document.getElementById("length")
 
 slider.addEventListener("input", function() {
@@ -40,22 +26,7 @@ slider.addEventListener("input", function() {
   }
 })
 
-
 const modal = document.getElementById("modal")
-const letter = `
-My Dearest,
-
-I think of you constantly, my love. Every morning when I wake up and every night before I go to sleep, my heart is filled with thoughts of you. I never knew it was possible to feel such a deep, unwavering love for another person, until I met you.
-
-You are the most beautiful thing I have ever seen. Your eyes sparkle and your smile lights up the room. I could get lost in your presence forever.
-
-You bring out the best in me. When I am with you, I feel like I can do anything. I feel confident and brave and ready to take on the world. I'm so thankful for the way you encourage and support me.
-
-The love I have for you is so immense and so powerful, it's hard to put into words. All I know is that I want to spend the rest of my life with you. I want to make memories together, laugh together and explore the world together. I want to love you, cherish you, and build a life together.
-
-Forever yours,
-
-[Your Name]`
 generateBtn.addEventListener("click", (event)=> {
   btnAction(event)
 })
@@ -67,6 +38,7 @@ function btnAction(event) {
   receiver =  document.getElementById("receiver").value
   notes =  document.getElementById("notes").value
   length =  document.getElementById("length").value
+  console.log(letterInfo)
 
   fetch("https://api.openai.com/v1/completions", {
     method: "POST",
@@ -74,22 +46,17 @@ function btnAction(event) {
       'Authorization': `Bearer ${secretKey.key}`,
       'Content-Type': "application/json",
     },
-   body: JSON.stringify({
-      "prompt": `
-      Write a love letter ${length} words long addressed from ${giver} to ${receiver}. Assume this is the innerHTML of an HTML div element and format accordingly with p and br elements. Do not include br element in the signoff!
-      Additional requirements/notes:
-      ${notes}
-      `,
+    body: JSON.stringify({
+      "prompt": getPrompt(length, giver, receiver, notes),
       "max_tokens": 250,
       "model": "text-davinci-003",
     })
   })
   .then(request => request.json())
   .then(data => {
-    console.log(data)
     const loveText = data['choices'][0].text
-    let wordCount = loveText.split(" ").length
-    renderModel(modal, data)
+    const wordCount = loveText.split(" ").length
+    renderModal(modal, data)
     console.log("Word count: " + wordCount)
   })
 
@@ -97,20 +64,24 @@ function btnAction(event) {
   modal.style.display = "block"
 }
 
-function renderModel(modalElement, data) {
-  modal.innerHTML = data['choices'][0].text
+function renderModal(modalElement, data) {
+  const deleteModalBtnHtml = `<button type="button" id="close-modal"></button>`
+  modal.innerHTML = data['choices'][0].text + deleteModalBtnHtml
   modalElement.style.display = "block"
 
+
   // For deleting the modal
-  const deleteModalBtn = document.createElement("button").setAttribute("id", "close-modal-btn")
+  const deleteModalBtn = document.getElementById("close-modal")
   deleteModalBtn.addEventListener("click", function() {
     modal.style.display = "none"
   })
 }
 
-function getPrompt(nameValue, notesValue) {
-  return `
-  Write me a love letter for ${nameValue}. The love letter must be about the following things:
-  ${notesValue}
+function getPrompt(length, giver, receiver, notes) {
+  const prompt = `
+    Write a love letter ${length} words long addressed from ${giver} to ${receiver}. Assume this is the innerHTML of an HTML div element and format accordingly with p and br elements. Do not include any br element in the sign-off! All text should be inside a p element but not inside a div.
+    Additional requirements/notes:
+    ${notes}
   `
+  return prompt
 }
